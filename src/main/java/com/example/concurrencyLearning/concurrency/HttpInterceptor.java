437648dd6b82e2a504a3a88bbc4f79228539bd8f
JayVae <919433524@qq.com>
2021-04-02 22:40:59 +0800

    refresh

---
+++ src/main/java/com/example/concurrencyLearning/concurrency/HttpInterceptor.java
@@ -0,0 +1,26 @@
+package com.example.concurrencyLearning.concurrency;
+
+import com.example.concurrencyLearning.concurrency.example.threadLocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class HttpInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("preHandle");
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        RequestHolder.remove();
+        log.info("requestHolder {}",RequestHolder.getId());
+        log.info("afterCompletion");
+        return;
+    }
+}
---
+++ src/main/java/com/example/concurrencyLearning/concurrency/example/package-info.java
@@ -0,0 +1,1 @@
+package com.example.concurrencyLearning.concurrency.example;
